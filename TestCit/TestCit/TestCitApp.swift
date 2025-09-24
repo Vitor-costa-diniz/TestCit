6b059abd86480287253586cd74bb29bc5164ec8a
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2025-09-24 16:33:25 -0300

    crete project

---
+++ TestCit/TestCit/TestCitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCitApp.swift
+//  TestCit
+//
+//  Created by Vitor Costa on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TestCitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
